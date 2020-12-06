16285e7cb2fcc09cd0b96911b95a9f5dcb4f7c8d
Ren4t <ren4tab@yandex.ru>
2020-12-06 20:18:04 +0300

    homework_1

--- core/src/ru/home/game/StarGame.java
+++ core/src/ru/home/game/StarGame.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class StartGame extends ApplicationAdapter {
+public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -18,7 +18,7 @@ public class StartGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.1f, 0.5f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
